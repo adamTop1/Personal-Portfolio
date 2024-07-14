bcd4215c63d13608751df4a41c21639ea46759ca
Adam <adamwasylak1@gmail.com>
2024-07-14 23:24:08 +0200

    improved styling

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,14 +3,13 @@ import { FlipWords } from './ui/flip-words'
 
 const Header = () => {
 	return (
-		<div className='flex w-full h-screen py-[200px] px-[200px] '>
+		<div className='flex w-full py-[200px] px-[200px] '>
 			<div className='flex flex-col items-start font-bold text-white text-8xl '>
 				<h1>Hello</h1>
 				<div>
-					I&apos;m <FlipWords words={[`Adam  W`, 'a programmer ']} className='text-sky-600' />
+					I&apos;m <FlipWords words={[`Adam  Wasylak`, 'a fullstack developer ']} duration={5000} className='text-sky-600' />
 				</div>
-                <p className='pt-10 text-sm max-w-[500px]'>I‘m a 18-year-old programmer specializing in backend development, aspiring to become a full-stack developer. I‘m passionate about programming, teamwork, and creating innovative solutions with attractive user interfaces, eager to tackle new challenges and collaborate with fellow technology enthusiasts.
-                My Journey</p>
+                <p className='pt-10 text-sm max-w-[500px]'>I‘m a 18-year-old programmer aspiring to become a full-stack developer. I‘m passionate about programming, teamwork, and creating innovative solutions with attractive user interfaces, eager to tackle new challenges and collaborate with fellow technology enthusiasts.</p>
 			</div>
 		</div>
 	)
--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -5,7 +5,8 @@ import { projects } from '@/constants'
 export function Skills() {
 	return (
 		<section className='flex flex-col items-center' id='skills'>
-            <h2 className='text-6xl font-bold text-slate-200'>SKILLS</h2>
+			<div className='w-8/12 h-[1px] bg-white'></div>
+            {/* <h2 className='text-6xl font-bold text-slate-200'>SKILLS</h2> */}
 			<div className='max-w-5xl px-8 mx-auto'>
 				<HoverEffect items={projects} />
 			</div>
